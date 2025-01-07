c9088e335262077a42c3017262e79fe89ecbc0b9
Ryokuman <ryokuman@naver.com>
2025-01-07 18:44:56 +0900

    feat : pipelines

---
+++ src/pipelines/request-review.ts
@@ -0,0 +1,12 @@
+import type { RequestReviewContext } from "../types/base-context";
+import { Pipeline } from "../pipeline";
+import { validateConfig } from "../middlewares/validate-config";
+import { validateNotionTask } from "../middlewares/validate-notion-task";
+import { selectReviewers } from "../middlewares/select-reviewers";
+
+export class RequestReviewPipeline extends Pipeline<RequestReviewContext> {
+  constructor(context: RequestReviewContext) {
+    super(context);
+    this.use(validateConfig).use(validateNotionTask).use(selectReviewers);
+  }
+}
